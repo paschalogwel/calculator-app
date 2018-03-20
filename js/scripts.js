548629223022323cb2b1deb5a41318007820aebb
paschalogwel <paschalogwel@gmail.com>
2018-03-20 16:13:26 +0300

    adds other operations

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 var add=function(number1, number2) {
 return number1 + number2;
 };
+var subtract=function(number1, number2){
+  return number1 - number2;
+}
+var multiply=function(number1, number2) {
+  return number1 * number2;
+}
+var division=function(number1, number2) {
+  return number1/number2;
+}
+var modulus=function(number1, number2) {
+  return number1 % number2;
+}
 var number1=parseInt(prompt("enter a number"));
 var number2=parseInt(prompt("enter a number"));
-alert(add(number1 ,number2));
+var result=multiply(number1 ,number2);
+alert(result);
