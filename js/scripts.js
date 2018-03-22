815fd3fd6d7ce8d817526fffc447a70339581110
paschalogwel <paschalogwel@gmail.com>
2018-03-22 11:40:39 +0300

    adds a prevent default

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
-var add=function(number1, number2) {
-return number1 + number2;
+var add = function(number1, number2) {
+  return number1 + number2;
 };
-var subtract=function(number1, number2){
+var subtract = function(number1, number2) {
   return number1 - number2;
 }
-var multiply=function(number1, number2) {
+var multiply = function(number1, number2) {
   return number1 * number2;
 }
-var division=function(number1, number2) {
-  return number1/number2;
+var division = function(number1, number2) {
+  return number1 / number2;
 }
-var modulus=function(number1, number2) {
+var modulus = function(number1, number2) {
   return number1 % number2;
 }
-var number1=parseInt(prompt("enter a number"));
-var number2=parseInt(prompt("enter a number"));
-var result=multiply(number1 ,number2);
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault()
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    alert(result);
+  });
+});
